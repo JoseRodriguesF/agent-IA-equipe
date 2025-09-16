b853abd5372f7fcee9ca0bf6b6526987ebaf46b2
JoseRodriguesF <jose.antonio.220507@gmail.com>
2025-09-16 17:39:08 -0300

    fix: ajustes no CORS

--- Server.js
+++ Server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cors({
   origin: ['http://localhost:3001', 'http://localhost:3000'],
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  methods: 'POST',
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
